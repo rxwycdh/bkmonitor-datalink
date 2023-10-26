59b346314bac9cca78c8abbdd15de90603abc1a8
rxwycdh <huginzhang@tencent.com>
2023-10-26 10:52:53 +0800

    bugfix: 修复配置项不对应

--- pkg/bk-monitor-worker/store/dependentredis/redis.go
+++ pkg/bk-monitor-worker/store/dependentredis/redis.go
@@ -11,12 +11,13 @@ package dependentredis
 
 import (
 	"context"
-	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/config"
 	"time"
 
+	goRedis "github.com/go-redis/redis/v8"
+
+	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/bk-monitor-worker/config"
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/utils/logger"
 	redisUtils "github.com/TencentBlueKing/bkmonitor-datalink/pkg/utils/register/redis"
-	goRedis "github.com/go-redis/redis/v8"
 )
 
 type Instance struct {
@@ -36,7 +37,7 @@ func NewInstance(ctx context.Context) (*Instance, error) {
 			SentinelAddress:  config.StorageDependentRedisSentinelAddress,
 			MasterName:       config.StorageDependentRedisSentinelMasterName,
 			SentinelPassword: config.StorageDependentRedisSentinelPassword,
-			Password:         config.StorageRedisStandalonePassword,
+			Password:         config.StorageDependentRedisStandalonePassword,
 			Db:               config.StorageDependentRedisDatabase,
 			DialTimeout:      time.Duration(config.StorageDependentRedisDialTimeout) * time.Second,
 			ReadTimeout:      time.Duration(config.StorageDependentRedisReadTimeout) * time.Second,
